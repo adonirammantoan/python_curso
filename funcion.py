56bc87a180084d30803ce576f6c59afad9383d75
adonirammantoan <44199334+adonirammantoan@users.noreply.github.com>
2018-10-16 13:33:11 -0300

    Add files via upload

---
+++ funcion.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3	
+import sys 
+def main():
+	comando = float (sys.argv[1].lower())
+	mensagem = sys.argv[2].lower()
+	print (comando)
+	print (mensagem)
+
+if __name__=='__main__':
+	main()
